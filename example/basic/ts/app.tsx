741141e04bde798eab432f6a5c9d2c4332f77e95
Joris Blaak <joris@label305.com>
2017-04-06 19:50:07 +0200

    Setup of development environment

---
+++ example/basic/ts/app.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {TheForm} from './components/the_form';
+
+const element = document.getElementById('container');
+
+ReactDOM.render(<TheForm/>, element);
---
+++ example/basic/ts/components/the_form.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export class TheForm extends React.Component<{}, {}> {
+    render() {
+        return <div>Hi</div>
+    }
+}
