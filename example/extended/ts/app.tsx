36a002932b67ce15558c6e00ef8a77b5e8f2fd5a
Joris Blaak <joris@label305.com>
2017-04-14 14:46:31 +0200

    Removed temporary examples

--- example/extended/ts/app.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Form from './form';
-
-const element = document.getElementById('container');
-
-ReactDOM.render(<Form/>, element);
--- example/large/ts/app.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import TheForm from './components/the_form';
-
-const element = document.getElementById('container');
-
-ReactDOM.render(<TheForm/>, element);
